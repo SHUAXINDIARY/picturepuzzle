9aced1b8417e336862884cecb28c7d4e93f5b250
SHUAXIN <shuaxinjs@qq.com>
2024-12-13 17:46:42 +0800

    feat: 基本布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-import "./App.css";
+import { useEffect, useRef } from "react";
+import "./libary/packery.js";
+
+import a from "./assets/testImage/006bAzwAgy1hpi6dijal4j35ie2q2b2c.jpg";
+import b from "./assets/testImage/0074J156ly1hplq1w0odrj31vw3pce82.jpg";
+import c from "./assets/testImage/008rZrLhgy1hr0jn3g09aj31mz17r7wh.jpg";
+import d from "./assets/testImage/20240613-121606.jpeg";
+
+const IMG_LIST = [a, b, c, d, a, b, c, d, a, b, c, d] as string[];
 
 function App() {
+  const imgContainer = useRef(null);
+  useEffect(() => {
+    if (imgContainer.current) {
+      setTimeout(() => {
+        // @ts-ignore
+        new Packery(imgContainer.current, {
+          itemSelector: ".imgItem",
+          gutter: 0,
+          percentPosition: true,
+        });
+      }, 100);
+    }
+  }, [imgContainer.current]);
   return (
-    <>
-      <div>
-        <button className="btn">Button</button>
+    <div className="w-screen h-screen bg-red-50 flex justify-center items-center overflow-hidden">
+      <div className="card bg-white w-10/12 h-5/6 shadow-xl">
+        <div
+          className="card-body relative w-full h-auto overflow-hidden imageContainer"
+          ref={imgContainer}
+        >
+          {IMG_LIST.map((item) => {
+            return (
+              <img
+                src={item}
+                key={item + Math.random() * 1000}
+                className="absolute h-1/4 object-cover imgItem"
+              />
+            );
+          })}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
